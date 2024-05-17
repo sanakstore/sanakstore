4616b32d249149af1a3479f0d7eedb1a656c373d
sanakstore <144905341+sanakstore@users.noreply.github.com>
2024-05-17 21:05:57 +0800

    sanak store

--- update.sh
+++ update.sh
@@ -48,7 +48,7 @@ res1() {
 netfilter-persistent
 clear
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e " \e[1;97;101m          UPDATE SCRIPT JULAK BANTUR       \e[0m"
+echo -e " \e[1;97;101m          UPDATE SCRIPT SANAK STORE       \e[0m"
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e ""
 echo -e "  \033[1;91m update script service\033[1;37m"
@@ -56,5 +56,7 @@ fun_bar 'res1'
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e ""
 echo -e " Done "
-
+echo ""
+read -n 1 -s -r -p "Press any key to back on menu"
+menu
 ###########- COLOR CODE -##############
